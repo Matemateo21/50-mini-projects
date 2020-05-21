c0dd988621f475b0d5caeb19178db75381eee16c
Matemateo21 <64185774+Matemateo21@users.noreply.github.com>
2020-05-21 23:47:09 +0200

    added chat functionality

---
+++ PROJECT-11-chatroom /scripts/app.js
@@ -0,0 +1,23 @@
+
+const chatList = document.querySelector('.chat-list')
+const newChatForm = document.querySelector('.new-chat')
+
+
+
+//add a new chat
+newChatForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chatroom.addChat(message)
+        .then(() => newChatForm.reset())
+        .catch(err => console.log(err));
+})
+//class instances 
+const chatUI = new ChatUI(chatList);
+const chatroom = new Chatroom('general', 'mateusz');
+
+
+//get chats and render 
+chatroom.getChats(data => chatUI.render(data));
+
+
