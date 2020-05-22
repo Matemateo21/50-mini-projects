309aaee91db6bf09c35209579a4b25c154d739f0
Matemateo21 <64185774+Matemateo21@users.noreply.github.com>
2020-05-22 11:46:53 +0200

    added class chatroom and full funcionality

--- PROJECT-11-chatroom /scripts/app.js
+++ PROJECT-11-chatroom /scripts/app.js
@@ -1,7 +1,9 @@
 
-const chatList = document.querySelector('.chat-list')
-const newChatForm = document.querySelector('.new-chat')
-
+const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
+const rooms = document.querySelector('.chat-rooms');
 
 
 //add a new chat
@@ -11,10 +13,31 @@ newChatForm.addEventListener('submit', e => {
     chatroom.addChat(message)
         .then(() => newChatForm.reset())
         .catch(err => console.log(err));
+
+});
+
+//update username
+newNameForm.addEventListener('submit', e => {
+    e.preventDefault();
+    //update name via chatroom
+    const newName = newNameForm.name.value.trim();
+    chatroom.updateName(newName);
+    //reset the form
+    newNameForm.reset();
+    // show then hide the update message
+    updateMssg.innerText = `Your name was updated to ${newName}`;
+    setTimeout(() => updateMssg.innerText = '', 3000);
+});
+
+//update the chat room
+rooms.addEventListener('click', e => {
+    console.log(e);
 })
+// ceck local storage for a name
+const username = localStorage.username ? localStorage.username : 'noName'
 //class instances 
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom('general', 'mateusz');
+const chatroom = new Chatroom('general', username);
 
 
 //get chats and render 
--- PROJECT-11-chatroom /scripts/ui.js
+++ PROJECT-11-chatroom /scripts/ui.js
@@ -6,11 +6,14 @@ class ChatUI {
     constructor(list) {
         this.list = list;
     }
+    clear() {
+        this.list.innerHTML = '';
+    }
     render(data) {
         const when = dateFns.distanceInWordsToNow(
             data.created_at.toDate(),
             { addSuffix: true }
-        )
+        );
         const html = `
         <li class="list-group-item">
         <span class="username">${data.username}</span>
